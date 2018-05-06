db698e420551e51e06363fdd859baa31a88927de
Jared Rickert <jaredrickert52@gmail.com>
2018-05-06 16:15:44 -0500

    added responsive styling to about

--- src/About/index.tsx
+++ src/About/index.tsx
@@ -14,57 +14,68 @@ export class About extends React.Component<IAboutProps, {}> {
 
     return (
       <div className="About">
-        <h1 className="About-header">About Me</h1>
-        <p>
-          To keep it simple, I am a fullstack developer currently living in
-          Minnesota looking to make my next career move. Please feel free to{" "}
-          <Link to="/contact">contact me</Link>.
-        </p>
+        <div className="About-section">
+          <p>About Me</p>
+        </div>
+        <div className="About-header">
+          <p>
+            To keep it simple, I am a fullstack developer currently living in
+            Minnesota looking to make my next career move. Please feel free to{" "}
+            <Link to="/contact">contact me</Link>.
+          </p>
 
-        <p>
-          I am a self taught programmer who has been programming as a hobby for{" "}
-          {years}+ years developing anything from writing my own desktop
-          experience on Linux, arduino projects, or writing simple games (mostly
-          command line). Nowadays, my extra free time is spent building things
-          on the web.
-        </p>
+          <p>
+            I am a self taught programmer who has been programming as a hobby
+            for over {years}+ years. I originally studied electrical engineering
+            at St Cloud State University but later switch to computer science as
+            I relized my passion was in coding.
+          </p>
 
-        <p>
-          When I am not coding and old man winter finally decides to leave
-          Minnesota, you can often find me going for a quick run.
-        </p>
+          <p>
+            Most of my early projects where developing my desktop experience on
+            Linux, arduino projects, and writing sample games (mostly command
+            line). Nowadays I spend most of time build things on the web.
+          </p>
 
-        <hr />
+          <p>
+            When I am not coding and old man winter finally decides to leave
+            Minnesota, you can often find me going for a quick run.
+          </p>
+        </div>
 
-        <h2>Relevant Skills</h2>
-        <h3>Frontend</h3>
-        <ul>
-          <li className="uncommon">HTML5</li>
-          <li className="epic">CSS3</li>
-          <li className="uncommon">Threejs</li>
-          <li className="epic">JavaScript</li>
-          <li className="epic">TypeScript</li>
-        </ul>
+        <div className="About-section">
+          <p>Mad Skillz</p>
+        </div>
+        <div className="About-content">
+          <h3>Frontend</h3>
+          <ul>
+            <li className="uncommon">Threejs</li>
+            <li className="epic">HTML5 & CSS3</li>
+            <li className="epic">JavaScript</li>
+            <li className="legendary">TypeScript</li>
+          </ul>
 
-        <hr />
+          <h3>Backend</h3>
+          <ul>
+            <li className="uncommon">Node</li>
+            <li className="epic">Postgresql</li>
+            <li className="epic">Go</li>
+            <li className="legendary">Python</li>
+            <li className="legendary">Linux</li>
+          </ul>
+        </div>
 
-        <h3>Backend</h3>
-        <ul>
-          <li className="legendary">Python</li>
-          <li className="uncommon">Node</li>
-          <li className="legendary">Linux</li>
-          <li className="epic">Postgresql</li>
-        </ul>
-
-        <hr />
-
-        <h2>Books I have read</h2>
-        <ul>
-          <li>Code Complete 2</li>
-          <li>Clean Code</li>
-          <li>Dragon compiler book</li>
-          <li>SICP</li>
-        </ul>
+        <div className="About-section">
+          <p>Books I have read</p>
+        </div>
+        <div className="About-content">
+          <ul>
+            <li>Code Complete 2</li>
+            <li>Clean Code</li>
+            <li>Compilers: Principles, Techniques, and Tools. (in progress)</li>
+            <li>SICP</li>
+          </ul>
+        </div>
       </div>
     );
   }
