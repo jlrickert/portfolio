1ec3bb2b44a242c0d558237c0f96dee08b642d0b
Jared Rickert <jaredrickert52@gmail.com>
2018-05-07 16:11:22 -0500

    added picture to home page

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,19 +1,21 @@
 import * as React from "react";
+import picUrl from "./pic.jpg";
 
 import "./styles.css";
 
-export interface IHomeProps {
-  className?: string;
-}
+export interface IHomeProps {}
 
 export class Home extends React.Component<IHomeProps, {}> {
   public render(): React.ReactElement<HTMLDivElement> {
     return (
       <section className="Home">
-        <div />
-        <h1 className="Home-logo">Jared Rickert</h1>
-        <p className="u-fadein-3">Fullstack Developer</p>
-        <svg>How to Use this?</svg>
+        <div className="Home-contents">
+          <div className="Home-portrait">
+            <img src={picUrl} />
+          </div>
+          <h1 className="Home-logo">Jared Rickert</h1>
+          <p className="u-fadein-3">Fullstack Developer</p>
+        </div>
       </section>
     );
   }
