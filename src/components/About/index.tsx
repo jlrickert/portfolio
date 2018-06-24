7b844972e7b9d419fd1ce799fda2fd6311c1610d
Jared Rickert <jaredrickert52@gmail.com>
2018-06-24 16:59:26 -0500

    updated about and projects info

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -2,21 +2,60 @@ import * as React from "react";
 import { Link } from "react-router-dom";
 import * as Styles from "./about.module.css";
 
-const yearsProgramming = new Date().getFullYear() - 2012;
+const yearsProgramming = new Date().getFullYear() - 2013;
 
 const main = (
   <div className={Styles.Main}>
-    <h1>A Passion for Coding</h1>
+    <h1>About</h1>
     <p>
-      Hi there, I am a software engineer who has been programming ever since I
-      stumbled upon the wonderful world of programming about {yearsProgramming}{" "}
-      years ago. I am always continuously learning and pushing towards mastery
-      of my craft.
+      Hi there, I am a software engineer with a passion for all things
+      opensource currently living in Minneapolis, MN.
     </p>
     <p>
-      When I am not coding you can often find me outside on a quick run or
-      hanging out at the cabin.
+      I am a recent college student currently pursing an entry into the world of
+      software development. I have done many projects both in and outside of
+      class from everything from embeded systems to full stack web applications.
+      My long term goals is to do development on blockchain. When I am not
+      coding I enjoy going for a long run, or canoing at the cabin.
     </p>
+  </div>
+);
+
+const services = (
+  <div className={Styles.Services}>
+    <h1>Services</h1>
+    <p>
+      Currently I am willing to accept small short term freelance projects so
+      feel free to{" "}
+      <Link to="contact">
+        <b>contact</b>
+      </Link>{" "}
+      me.
+    </p>
+    <div className={Styles.Service}>
+      <h3>Front End Development</h3>
+      <p>
+        Front End Development is building out the visual components of a
+        website. Using HTML, CSS, and Javascript, I build fast, interactive
+        websites.
+      </p>
+    </div>
+    <div className={Styles.Service}>
+      <h3>Back End Development</h3>
+      <p>
+        Back End Development is building out the business modal of a website.
+        Using Python or Nodejs and a storage solution, I build quick solutions
+        to get your business idea out quickly.
+      </p>
+    </div>
+    <div className={Styles.Service}>
+      <h3>Fullstack Web Development</h3>
+      <p>
+        Full Stack Web devepment does the job of both the Back End and Front End
+        development for a website. Using things such as Django or MERN stack, I
+        can quickly get a mvp to kickstart your idea.
+      </p>
+    </div>
   </div>
 );
 
@@ -27,21 +66,39 @@ const skillz = (
       <div className={Styles.SkillCategory}>
         <h3>Frontend</h3>
         <ul>
-          <li className={Styles.Uncommon}>Threejs</li>
-          <li className={Styles.Epic}>HTML5 & CSS3</li>
-          <li className={Styles.Epic}>JavaScript</li>
-          <li className={Styles.Legendary}>TypeScript</li>
+          <li className={Styles.Uncommon}>Threejs (3/10)</li>
+          <li className={Styles.Legendary}>JavaScript (8/10)</li>
+          <li className={Styles.Legendary}>
+            React & Redux with Typescript (9/10)
+          </li>
+          <li className={Styles.Legendary}>Typescript (10/10)</li>
+          <li className={Styles.Legendary}>HTML5 & CSS3 (10/10)</li>
         </ul>
       </div>
 
       <div className={Styles.SkillCategory}>
         <h3>Backend</h3>
         <ul>
-          <li className={Styles.Uncommon}>Node</li>
-          <li className={Styles.Epic}>Postgresql</li>
-          <li className={Styles.Epic}>Go</li>
-          <li className={Styles.Legendary}>Python</li>
-          <li className={Styles.Legendary}>Linux</li>
+          <li className={Styles.Uncommon}>PostgreSQL (6/10)</li>
+          <li className={Styles.Uncommon}>Go (6/10)</li>
+          <li className={Styles.Epic}>Node (7/10)</li>
+          <li className={Styles.Epic}>Flask (7/10)</li>
+          <li className={Styles.Epic}>Django & DRF (7/10)</li>
+          <li className={Styles.Legendary}>Linux (8/10)</li>
+          <li className={Styles.Legendary}>Python (10/10)</li>
+        </ul>
+      </div>
+      <div className={Styles.SkillCategory}>
+        <h3>Other skills and languages</h3>
+        <ul>
+          <li className={Styles.Common}>Java (3/10)</li>
+          <li className={Styles.Common}>Scala (4/10)</li>
+          <li className={Styles.Uncommon}>Clojure (5/10)</li>
+          <li className={Styles.Uncommon}>Scheme (5/10)</li>
+          <li className={Styles.Uncommon}>Elm (5/10)</li>
+          <li className={Styles.Epic}>Haskell (6/10)</li>
+          <li className={Styles.Epic}>Rust (favorite) (7/10)</li>
+          <li className={Styles.Epic}>C/C++ (7/10)</li>
         </ul>
       </div>
     </div>
@@ -69,6 +126,7 @@ export class About extends React.Component<Props, {}> {
     return (
       <div className={Styles.About}>
         {main}
+        {services}
         {skillz}
         {books}
       </div>
--- src/components/Portfolio/index.tsx
+++ src/components/Portfolio/index.tsx
@@ -7,21 +7,41 @@ import { Project, Props as ProjectData } from "../Project";
 
 const projects: ProjectData[] = [
   {
+    name: "Quick Draw",
+    url: "https://github.com/chingu-voyage6/Bears-Team-08",
+    tech: ["React", "Redux", "Typescript", "MongoDB", "Nodejs", "canvas"],
+    category: ["full stack web development", "collaboration"],
+    shortDescription:
+      "A quick way to share ideas by collaboratively drawing it on a canvas.",
+    description: `I originally started this project as a means to quickly draw
+data structures to explain how things work in a remote environment. This latter
+became a project with a Chingu cohort for group 8 as we needed a project and I
+just barely got started it.
+
+The project is currently development phase as of June 2018. What we have planned
+is a way to share drawings via an url link where any one may fork or edit
+directly with authorization from the owner. Other things that we would like to
+add is collaborative drawing where multiple people may interact with a drawing
+at a time, although, this is not part of the mvp.
+      `,
+  },
+  {
     name: "portfolio",
     url: "https://github.com/jlrickert/portfolio",
     tech: ["typescript", "react"],
     category: ["frontend", "SEO"],
-    shortDescription: "My personal front page to the internet",
-    description: `
-I wanted a way to showcase the kinds of projects that I like to work on and also
-advertise my skillset. The very nature of this project means that It will always
-be a work in progress as my skills improve and interests change.
+    shortDescription: "My personal front page to the internet.",
+    description: `I wanted a way to showcase the kinds of projects that I like
+to work on and also advertise my skillset. The very nature of this project means
+that It will always be a work in progress as my skills improve and interests
+change.
 
 One challenge that I ran into was problems with bots not able to view the
 contents of the page. This is a problem when it come to SEO and linking to
 my page in places such as LinkedIn as it renders my front page as a plain
 white page. One possible solution is to write a webpack plugin that will
 pre render the page.  It could also be possible to render each of the routes.
+Another possible solution is to port this to gatsbyJS.
     `,
   },
   {
@@ -29,21 +49,22 @@ pre render the page.  It could also be possible to render each of the routes.
     url: "https://github.com/jlrickert/rusty",
     tech: ["rust"],
     category: ["ai", "halite2"],
-    shortDescription: "Bot written for the game halite 2",
-    description: `
-An experiment with rust by writing a bot to play the game halite 2. One major
-advantage that rust gave me is that my bot would never crash. As a result I
-won a few extra games because the other bots would occasionally crash.
+    shortDescription: "Bot written for the game halite 2.",
+    description: `An experiment with rust by writing a bot to play the game
+halite 2. One major advantage that rust gave me is that my bot would never
+crash. As a result I won a few extra games because the other bots would
+occasionally crash.
+
+The biggest challenge that I had was learning rust.
     `,
   },
   {
     name: "DTS",
     tech: ["python", "MS-SQL"],
     category: ["Automation"],
-    shortDescription: "Database that represents the state of the world",
-    description: `
-The goal of this project is to use a central table that describes the state of
-the world and to sync that state to the world.
+    shortDescription: "Database that represents the state of the world.",
+    description: `The goal of this project is to use a central table that
+describes the state of the world and to sync that state to the world.
 
 DTS is a python library where the user would describe a module that
 depends on a set a fields in the table and define functions that react to the
@@ -67,15 +88,15 @@ tree would be updated until both trees are equivalent.
     tech: ["python", "wxpython"],
     url: "https://github.com/jlrickert/form-monster",
     category: ["Desktop"],
-    shortDescription: "A declarative way to quickly generate forms",
-    description: `
-The goal of this project was to help new programmers create simple forms
-with wxpython without knowing any details about GUI development.
+    shortDescription: "A declarative way to quickly generate forms.",
+    description: `The goal of this project was to prototype a way to simplify
+creating forms with xwpython so new progrogrammers could develop these forms
+without knowledge of GUI development.
 
 Form monster takes a simple python dictionary that would describe all fields
-and their properties. Some examples of properties that a field may have is type
-of field such as number, text, choice, or checkbox, extra validation logic,
-optional or not, initial value, and more. 
+and their associated properties. Some examples of properties that a field may
+have is type of field such as number, text, choice, or checkbox, extra
+validation logic, optional or not, initial value, and more.
 
 When this description is passed to Form monster a generic GUI would be created.
 The client would then enter information and would be able to submit when all
@@ -85,13 +106,12 @@ information is validated.`,
     name: "Arduino Sound Recorder",
     tech: ["C", "ASM", "Arduino"],
     category: ["Hardware", "IOT"],
-    shortDescription: "Arduino sound recorder",
-    description: `
-This project involved creating a sound recorder using an Arduino Uno, SD card
-reader, and a custom microphone circuit. The reason for building it was to
-gather data points about sound in a high altitude balloon to determine if
-their were any effects on amplitude and frequency. Later this project turned
-into exploring the arduino further.
+    shortDescription: "Arduino sound recorder.",
+    description: `This project involved creating a sound recorder using an
+Arduino Uno, SD card reader, and a custom microphone circuit. The reason for
+building it was to gather data points about sound in a high altitude balloon to
+determine if their were any effects on amplitude and frequency. Later this
+project turned into exploring the arduino further.
 
 Originally it was built using the arduino libraries, but later switch to
 using only avr-toolchain and building everything from scratch that the arduino
@@ -104,24 +124,22 @@ libraries provided.
     url: "https://github.com/jlrickert/ConnectFour",
     category: ["CLI"],
     shortDescription: "A command line game of Connect Four.",
-    description: `
-A remake of the classic Connect Four Game.  Similar to my Tic Tac Toe project it
-involved drawing the board on the commandline where it would take turns between
-two players. A player may be a dumb AI. A player would select were to place a
-chip by typing in a column. I would detect invalid moves and if a winner is
-present.`,
+    description: `A remake of the classic Connect Four Game.  Similar to my Tic
+Tac Toe project it involved drawing the board on the commandline where it would
+take turns between two players. A player may be a dumb AI. A player would select
+were to place a chip by typing in a column. It would detect invalid moves and if
+a winner is present.`,
   },
   {
     name: "Tic Tac Toe",
     tech: ["C", "TCP"],
     category: ["CLI", "Networking"],
     shortDescription:
-      "A command line program of the classic Tic Tac Toe, including networked multiplayer",
-    description: `
-This is probably the first complete programs I have ever written.  It is a
-simple remake of the classic tic tac toe game that draws the board on the
-command line. Selecting the cell was a simple as typing in the Cell row and
-column, ex A3. It would not allow invalid placements of chips.  Multiplayer
+      "A command line program of the classic Tic Tac Toe, including networked multiplayer.",
+    description: `This is probably the first complete programs I have ever
+written.  It is a simple remake of the classic tic tac toe game that draws the
+board on the command line. Selecting the cell was a simple as typing in the Cell
+row and column, ex A3. It would not allow invalid placements of chips.  Multiplayer
 was originally not planned but was later added to learn about networking.`,
   },
 ];
@@ -134,8 +152,9 @@ export class Portfolio extends React.Component<Props, {}> {
       <div className={Styles.Portfolio}>
         <h1>Portfolio</h1>
         <p>
-          This is just a sample of some of the projects that I have done over
-          the years. You may also find more stuff on{" "}
+          This is just a sample of some of the projects that I have completed
+          over the years. If you click on them you get more information about
+          the project. You may also find more stuff on{" "}
           <a href="https://github.com/jlrickert">github</a>.
         </p>
         <div className={Styles.Projects}>
